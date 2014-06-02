4d3c813a5f7dd57297e501a15dd311ca2427c3b3
Filip Jukic <filip@jukic.me>
2014-06-02 16:02:58 +0200

    order deployments by id

--- launcher/deployment/admin.py
+++ launcher/deployment/admin.py
@@ -11,7 +11,7 @@ class DeploymentModelAdmin(admin.ModelAdmin):
     list_display = ('deploy_id', 'project', 'deployed_app_url', 'email',
                     'status', 'launch_time', 'get_remaining_minutes')
     list_filter = ('status', 'project__name')
-    ordering = ['-launch_time', 'project']
+    ordering = ['-id', 'project']
 
     def deployed_app_url(self, obj):
         return '<a href="{0}">{0}</a>'.format(obj.url)
