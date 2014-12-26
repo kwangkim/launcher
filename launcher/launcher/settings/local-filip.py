5eab62b54a05a516df5342ba10033392f954fb59
Filip Jukic <filip@jukic.me>
2014-12-26 22:03:05 +0100

    disable actual mail sending in Filip's settings

--- launcher/launcher/settings/local-filip.py
+++ launcher/launcher/settings/local-filip.py
@@ -5,11 +5,11 @@ DEBUG = True
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_HOST = 'mail.kset.org'
-EMAIL_HOST_USER = get_env_variable('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = get_env_variable('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'mail.kset.org'
+# EMAIL_HOST_USER = get_env_variable('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = get_env_variable('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
 
 DATABASES = {
     'default': {
