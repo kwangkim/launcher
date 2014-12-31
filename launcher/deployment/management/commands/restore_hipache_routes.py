163afa32ca0e1dd96332e7366c55d7f68489c8ad
Tomasz Zieliński <tomasz.zielinski@pyconsultant.eu>
2014-12-31 15:49:53 +0100

    Added management command for restoring app routes

---
+++ launcher/deployment/management/commands/restore_hipache_routes.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from deployment.models import Deployment
+
+
+class Command(BaseCommand):
+    help = 'Restores Hipache routes for all apps'
+
+    def info(self, message):
+        self.stdout.write(message)
+
+    def error(self, message):
+        self.stderr.write(message)
+
+    def handle(self, *args, **options):
+        active_apps = Deployment.objects.filter(status='Completed')
+        for app in active_apps:
+            app.restore(logger_instance=self)
+
+        expired_apps = Deployment.objects.filter(status='Expired')
+        for app in expired_apps:
+            app.expire(logger_instance=self)
